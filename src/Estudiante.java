5e3b89e09579d45ed65873d76237c038d36a61e2
Cristina <bau161260@uvg.edu.gt>
2018-07-16 23:49:05 -0600

    Cree clases para los niveles, grados, estudiantes y el main

---
+++ src/Estudiante.java
@@ -0,0 +1,26 @@
+public class Estudiante {
+    private String nivel;
+    private int Grado;
+    private String estudiante;
+    private long uniqueID;
+
+
+    public static final String Agregar_Nivel = ;
+    public static final int Agregar_Grado 
+
+    protected static int asignacionesNivel = 0;
+    protected static int getAsignacionesGrado = 0;
+
+    //static Scanner userinput = new Scanner(System.in);
+
+
+
+
+
+    public Estudiante(String nivel, int grado, String estudiante, long uniqueID) {
+        this.nivel = nivel;
+        this.Grado = grado;
+        this.estudiante = estudiante;
+        this.uniqueID = uniqueID;
+    }
+}
---
+++ src/Niveles.java
@@ -0,0 +1,4 @@
+public class Niveles {
+    private String nivel;
+
+}
